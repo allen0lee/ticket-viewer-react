fff98d208b38437ad57949db4b0db74061eefc80
Alan <allen0lee@gmail.com>
2021-05-12 15:25:37 +1000

    add test case of initial page

--- src/test/makeReqToApi.test.js
+++ src/test/makeReqToApi.test.js
@@ -1,7 +1,13 @@
 import makeReqToApi from '../lib/makeReqToApi.js'
 
+test("test valid response for setting up initial first page", async () => {
+  const url = "http://localhost:9292"
+  const res = await makeReqToApi(url)
+  expect("tickets" in res).toBe(true)
+})
+
 test("test valid response for tickets", async () => {
-  const url = "http://localhost:9292/tickets_list/page/1"
+  const url = "http://localhost:9292/tickets_list/page/2"
   const res = await makeReqToApi(url)
   expect("tickets" in res).toBe(true)
 })
